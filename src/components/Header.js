89e0d7a95f6eb91fc5bd9fb3a7281a34a2f8c2e6
Wyatt <wyattstephenson@rocketmail.com>
2023-08-24 13:23:32 -0600

    done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
+
+
+function Header ({onDarkModeClick, isDarkMode, setIsDarkMode}) {
+
+    return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+    <ShoppingList items={itemData} />
+  </div>
+    )
+
+}
+
+export default Header
